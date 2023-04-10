3ebed429a8b548c469a699dbb487e66d88e1a5ba
Hudson Holanda <hudsonholanda55@gmail.com>
2023-04-10 14:45:46 -0300

    feat: import and usage TaskList component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,11 +1,13 @@
 import { FormAddNewTask } from "./FormAddNewTask";
 import styles from './Main.module.css';
+import { TaskList } from "./TaskList";
 export function Main() {
   return (
     <main>
       <section>
         <div className={styles.container}>
           <FormAddNewTask />
+          <TaskList />
         </div>
       </section>
     </main>
