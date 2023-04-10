c9c3fd381b099fe9479c8b8610162bfd67a67f2c
Hudson Holanda <hudsonholanda55@gmail.com>
2023-04-10 16:00:49 -0300

    feat: usage function to add new task

--- src/components/FormAddNewTask.tsx
+++ src/components/FormAddNewTask.tsx
@@ -2,10 +2,25 @@ import { PlusCircle } from '@phosphor-icons/react';
 import styles from './FormAddNewTask.module.css';
 import { Input } from './Input';
 import { ButtonAddNewTask } from './ButtonAddNewTask';
-export function FormAddNewTask() {
+import { FormEvent, useState } from 'react';
+
+interface FormAddNewTaskProps {
+  onAddNewTask: (description: string) => void;
+}
+
+export function FormAddNewTask({ onAddNewTask } : FormAddNewTaskProps) {
+  const [description, setDescription] = useState('');
+
+  function handleAddNewTask(e : FormEvent) {
+    e.preventDefault();
+    onAddNewTask(description)
+  }
+
   return (
-    <form className={styles.form}>
+    <form onSubmit={handleAddNewTask} className={styles.form}>
       <Input 
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
         placeholder='Adicione uma nova tarefa'
       />
       <ButtonAddNewTask type='submit' />
